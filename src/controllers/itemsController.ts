b2355623931a8336f14ad45dd7239ededac69587
Alan Vasconcelos <alandbh@gmail.com>
2022-02-28 22:29:17 -0300

    Updating modules

--- src/controllers/itemsController.ts
+++ src/controllers/itemsController.ts
@@ -15,7 +15,7 @@ class ItemsController {
             return {
                 id: item.id,
                 title: item.title,
-                image_url: `http://192.168.0.6:3333/uploads/${item.image}`,
+                image_url: `http://192.168.1.137:3333/uploads/${item.image}`,
             };
         });
 
